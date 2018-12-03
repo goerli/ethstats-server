32c56d54c6b0cd9367208e80135ee628acaa2231
5chdn <5chdn@users.noreply.github.com>
2018-12-03 13:11:51 +0100

    server: allow for poa and pow network stats

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,64 @@
-var src = 'src/';
-var dest = 'dist/';
+var src_poa = 'src/poa/';
+var dest_poa = 'dist/poa/';
 
-var scripts = [
-	'src/js/app.js',
-	'src/js/controllers.js',
-	'src/js/filters.js',
-	'src/js/directives.js',
-	'src/js/script.js'
+var scripts_poa = [
+	'src/poa/js/app.js',
+	'src/poa/js/controllers.js',
+	'src/poa/js/filters.js',
+	'src/poa/js/directives.js',
+	'src/poa/js/script.js'
 ];
 
-var vendor = [
-	'dist/js/lib/jquery-1.11.3.min.js',
-	'dist/js/lib/bootstrap.min.js',
-	'dist/js/lib/angular.min.js',
-	'dist/js/lib/ngStorage.min.js',
-	'dist/js/lib/lodash.min.js',
-	'dist/js/lib/d3.min.js',
-	'dist/js/lib/d3.tip.min.js',
-	'dist/js/lib/topojson.min.js',
-	'dist/js/lib/datamaps.min.js',
-	'dist/js/lib/moment.min.js',
-	'dist/js/lib/moment.en.min.js',
-	'dist/js/lib/toastr.min.js',
-	'dist/js/lib/jquery.sparkline.min.js',
-	'dist/js/lib/primus.min.js'
+var vendor_poa = [
+	'dist/poa/js/lib/jquery-1.11.3.min.js',
+	'dist/poa/js/lib/bootstrap.min.js',
+	'dist/poa/js/lib/angular.min.js',
+	'dist/poa/js/lib/ngStorage.min.js',
+	'dist/poa/js/lib/lodash.min.js',
+	'dist/poa/js/lib/d3.min.js',
+	'dist/poa/js/lib/d3.tip.min.js',
+	'dist/poa/js/lib/topojson.min.js',
+	'dist/poa/js/lib/datamaps.min.js',
+	'dist/poa/js/lib/moment.min.js',
+	'dist/poa/js/lib/moment.en.min.js',
+	'dist/poa/js/lib/toastr.min.js',
+	'dist/poa/js/lib/jquery.sparkline.min.js',
+	'dist/poa/js/lib/primus.min.js'
 ];
 
-var styles = [
+var styles_poa = [
+	'bootstrap.min.css',
+	'minimal-icons-embedded.css',
+	'toastr.min.css',
+	'style.css'
+];
+
+var src_pow = 'src/pow/';
+var dest_pow = 'dist/pow/';
+
+var scripts_pow = [
+	'src/pow/js/app.js',
+	'src/pow/js/controllers.js',
+	'src/pow/js/filters.js',
+	'src/pow/js/directives.js',
+	'src/pow/js/script.js'
+];
+
+var vendor_pow = [
+	'dist/pow/js/lib/jquery-1.11.3.min.js',
+	'dist/pow/js/lib/bootstrap.min.js',
+	'dist/pow/js/lib/angular.min.js',
+	'dist/pow/js/lib/lodash.min.js',
+	'dist/pow/js/lib/d3.min.js',
+	'dist/pow/js/lib/d3.tip.min.js',
+	'dist/pow/js/lib/moment.min.js',
+	'dist/pow/js/lib/moment.en.min.js',
+	'dist/pow/js/lib/toastr.min.js',
+	'dist/pow/js/lib/jquery.sparkline.min.js',
+	'dist/pow/js/lib/primus.min.js'
+];
+
+var styles_pow = [
 	'bootstrap.min.css',
 	'minimal-icons-embedded.css',
 	'toastr.min.css',
@@ -37,12 +69,15 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		clean: {
-			build: ['dist'],
-			cleanup_js: ['dist/js/*.*', '!dist/js/netstats.*'],
-			cleanup_css: ['dist/css/*.css', '!dist/css/netstats.*.css'],
+			build_poa: ['dist/poa'],
+			cleanup_js_poa: ['dist/poa/js/*.*', '!dist/poa/js/netstats.*'],
+			cleanup_css_poa: ['dist/poa/css/*.css', '!dist/poa/css/netstats.*.css'],
+			build_pow: ['dist/pow'],
+			cleanup_js_pow: ['dist/pow/js/*.*', '!dist/pow/js/netstats.*'],
+			cleanup_css_pow: ['dist/pow/css/*.css', '!dist/pow/css/netstats.*.css']
 		},
 		jade: {
-			build: {
+			build_poa: {
 				options: {
 					data: {
 						debug: false,
@@ -50,94 +85,183 @@ module.exports = function(grunt) {
 					}
 				},
 				files: {
-					'dist/index.html': 'src/views/index.jade'
+					'dist/poa/index.html': 'src/poa/views/index.jade'
+				}
+			},
+			build_pow: {
+				options: {
+					data: {
+						debug: false,
+						pretty: true
+					}
+				},
+				files: {
+					'dist/pow/index.html': 'src/pow/views/index.jade'
 				}
 			}
 		},
 		copy: {
-			build: {
+			build_poa: {
 				files: [
 					{
 						expand: true,
-						cwd: 'src/fonts/',
-						src: ['*.*'],
-						dest: 'dist/fonts/',
+						cwd: 'src/poa/fonts/',
+						src: ['minimal-*.*'],
+						dest: 'dist/poa/fonts/',
 						filter: 'isFile'
 					},
 					{
 						expand: true,
-						cwd: 'src/images/',
-						src: ['*.*'],
-						dest: 'dist/',
+						cwd: 'src/poa/images/',
+						src: ['*.ico'],
+						dest: 'dist/poa/',
 						filter: 'isFile'
 					},
 					{
 						expand: true,
-						cwd: 'src/css/',
-						src: styles,
-						dest: 'dist/css/',
+						cwd: 'src/poa/css/',
+						src: styles_poa,
+						dest: 'dist/poa/css/',
 						filter: 'isFile'
 					},
 					{
 						expand: true,
-						cwd: 'src/js/lib/',
+						cwd: 'src/poa/js/lib/',
 						src: ['*.*'],
-						dest: 'dist/js/lib'
+						dest: 'dist/poa/js/lib'
+					}
+				]
+			},
+			build_pow: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/pow/fonts/',
+						src: ['minimal-*.*'],
+						dest: 'dist/pow/fonts/',
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'src/pow/images/',
+						src: ['*.ico'],
+						dest: 'dist/pow/',
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'src/pow/css/',
+						src: styles_pow,
+						dest: 'dist/pow/css/',
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'src/pow/js/lib/',
+						src: ['*.*'],
+						dest: 'dist/pow/js/lib'
 					}
 				]
 			}
 		},
 		cssmin: {
-			build: {
+			build_poa: {
 				files: [{
 					expand: true,
-					cwd: 'dist/css',
+					cwd: 'dist/poa/css',
 					src: ['*.css', '!*.min.css'],
-					dest: 'dist/css/'
+					dest: 'dist/poa/css/'
+				}]
+			},
+			build_pow: {
+				files: [{
+					expand: true,
+					cwd: 'dist/pow/css',
+					src: ['*.css', '!*.min.css'],
+					dest: 'dist/pow/css/'
 				}]
 			}
 		},
 		concat: {
-			vendor: {
+			vendor_poa: {
 				options: {
 					souceMap: false,
 					sourceMapIncludeSources: true,
-					sourceMapIn: ['dist/js/lib/*.map']
+					sourceMapIn: ['dist/poa/js/lib/*.map']
 				},
-				src: vendor,
-				dest: 'dist/js/vendor.min.js'
+				src: vendor_poa,
+				dest: 'dist/poa/js/vendor.min.js'
 			},
-			scripts : {
+			scripts_poa : {
 				options: {
 					separator: ';\n',
 				},
-				src: scripts,
-				dest: 'dist/js/app.js'
+				src: scripts_poa,
+				dest: 'dist/poa/js/app.js'
 			},
-			netstats: {
+			netstats_poa: {
 				options: {
 					sourceMap: false,
 					sourceMapIncludeSources: true,
-					sourceMapIn: ['dist/js/vendor.min.js.map', 'dist/js/app.min.js.map']
+					sourceMapIn: ['dist/poa/js/vendor.min.js.map', 'dist/poa/js/app.min.js.map']
 				},
-				src: ['<%= concat.vendor.dest %>', '<%= uglify.app.dest %>'],
-				dest: 'dist/js/netstats.min.js',
+				src: ['<%= concat.vendor_poa.dest %>', '<%= uglify.app_poa.dest %>'],
+				dest: 'dist/poa/js/netstats.min.js',
 				nonull: true
 			},
-			css: {
-				src: ['dist/css/*.min.css', 'dist/css/*.css'],
-				dest: 'dist/css/netstats.min.css'
+			css_poa: {
+				src: ['dist/poa/css/*.min.css', 'dist/poa/css/*.css'],
+				dest: 'dist/poa/css/netstats.min.css'
+			},
+			vendor_pow: {
+				options: {
+					sourceMap: false,
+					sourceMapIncludeSources: true,
+					sourceMapIn: ['dist/pow/js/lib/*.map']
+				},
+				src: vendor_pow,
+				dest: 'dist/pow/js/vendor.min.js'
+			},
+			scripts_pow : {
+				options: {
+					separator: ';\n',
+				},
+				src: scripts_pow,
+				dest: 'dist/pow/js/app.js'
+			},
+			netstats_pow: {
+				options: {
+					sourceMap: false,
+					sourceMapIncludeSources: true,
+					sourceMapIn: ['dist/pow/js/vendor.min.js.map', 'dist/pow/js/app.min.js.map']
+				},
+				src: ['<%= concat.vendor_pow.dest %>', '<%= uglify.app_pow.dest %>'],
+				dest: 'dist/pow/js/netstats.min.js',
+				nonull: true,
+			},
+			css_pow: {
+				src: ['dist/pow/css/*.min.css', 'dist/pow/css/*.css'],
+				dest: 'dist/pow/css/netstats.min.css'
 			}
 		},
 		uglify: {
-			app: {
+			app_poa: {
 				options: {
 					mangle: false,
 					sourceMap: false,
 					sourceMapIncludeSources: true
 				},
-				dest: 'dist/js/app.min.js',
-				src: ['<%= concat.scripts.dest %>']
+				dest: 'dist/poa/js/app.min.js',
+				src: ['<%= concat.scripts_poa.dest %>']
+			},
+			app_pow: {
+				options: {
+					mangle: false,
+					sourceMap: false,
+					sourceMapIncludeSources: true
+				},
+				dest: 'dist/pow/js/app.min.js',
+				src: ['<%= concat.scripts_pow.dest %>']
 			}
 		}
 	});
@@ -149,7 +273,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', ['clean:build', 'clean:cleanup_js', 'clean:cleanup_css', 'jade:build', 'copy:build', 'cssmin:build', 'concat:vendor', 'concat:scripts', 'uglify:app', 'concat:netstats', 'concat:css', 'clean:cleanup_js', 'clean:cleanup_css']);
-	grunt.registerTask('build', 'default');
-	grunt.registerTask('all', 'default');
+	grunt.registerTask('poa', ['clean:build_poa', 'clean:cleanup_js_poa', 'clean:cleanup_css_poa', 'jade:build_poa', 'copy:build_poa', 'cssmin:build_poa', 'concat:vendor_poa', 'concat:scripts_poa', 'uglify:app_poa', 'concat:netstats_poa', 'concat:css_poa', 'clean:cleanup_js_poa', 'clean:cleanup_css_poa']);
+	grunt.registerTask('pow', ['clean:build_pow', 'clean:cleanup_js_pow', 'clean:cleanup_css_pow', 'jade:build_pow', 'copy:build_pow', 'cssmin:build_pow', 'concat:vendor_pow', 'concat:scripts_pow', 'uglify:app_pow', 'concat:netstats_pow', 'concat:css_pow', 'clean:cleanup_js_pow', 'clean:cleanup_css_pow']);
+	grunt.registerTask('default', 'poa');
+	grunt.registerTask('build',   'default');
+	grunt.registerTask('all',   ['poa', 'pow']);
 };
---
+++ src/pow/js/script.js
@@ -0,0 +1,33 @@
+(function() {
+	$('body').on('mouseenter', '[data-toggle="tooltip"]', function( event ) {
+		$(this).tooltip('show');
+	}).on('mouseleave', '[data-toggle="tooltip"]', function( event ) {
+		$(this).tooltip('hide');
+	});
+
+	$.fn.sparkline.defaults.bar.height = 63;
+	$.fn.sparkline.defaults.bar.barWidth = 6;
+
+	if( $('body').width() < 600 )
+		$.fn.sparkline.defaults.bar.barWidth = 4;
+	else if( $('body').width() < 1200 )
+		$.fn.sparkline.defaults.bar.barWidth = 5;
+	$.fn.sparkline.defaults.bar.barSpacing = 1;
+	$.fn.sparkline.defaults.bar.tooltipClassname = 'jqstooltip';
+	$.fn.sparkline.defaults.bar.tooltipOffsetX = 0;
+	$.fn.sparkline.defaults.bar.tooltipFormat = $.spformat('<div class="tooltip-arrow"></div><div class="tooltip-inner">{{prefix}}{{value}} {{suffix}}</div>');
+	$.fn.sparkline.defaults.bar.colorMap = $.range_map({
+		'0:6': '#8be9fd',
+		'6:15': '#50fa7b',
+		'15:40': '#f1fa8c',
+		'40:60': '#ffb86c',
+		'60:': '#ff5555'
+	});
+
+	moment.relativeTimeThreshold('s', 60);
+	moment.relativeTimeThreshold('m', 60);
+	moment.relativeTimeThreshold('h', 24);
+	moment.relativeTimeThreshold('d', 28);
+	moment.relativeTimeThreshold('M', 12);
+
+})();
